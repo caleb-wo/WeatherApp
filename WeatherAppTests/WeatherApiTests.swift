c566985749ebe3990a51dfd2a235920c75339eef
caleb-wo <camawolfe52@icloud.com>
2025-09-18 23:08:33 -0600

    Commented out test. After research/learning, I'm going to refactor for a custom decodable type over [String: Any].

--- WeatherAppTests/WeatherApiTests.swift
+++ WeatherAppTests/WeatherApiTests.swift
@@ -12,11 +12,23 @@ struct WeatherApiTests {
 
     @Test("Check JSON GET request")
     func get7DayForecastJSONTest() async throws {
-        let apiService = WeatherApiService()
-        
-        do{
-            var data = try? await apiService.getForecastDictionary(from: "92336")
-        }
+//        let apiService = WeatherApiService()
+//        let data :[String: Any]?
+//        let JSON :[String: Any]
+//        var testFailed = false
+//        
+//        do{
+//            data = try? await apiService.getForecastDictionary(from: "92336")
+//        }
+//        
+//        guard let JSON = data else {
+//            testFailed = true
+//            return
+//        }
+//        
+//        if !testFailed{
+//            #expect(JSON != nil)
+//        }
     }
 
 }
