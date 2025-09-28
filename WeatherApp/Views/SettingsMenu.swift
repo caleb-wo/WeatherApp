b97d4dbbda7e05830ec9a66358ae58112aec6da8
calebwolfe <caleb-wolfe@gmx.com>
2025-09-28 17:40:33 -0600

    Creating SettingsMenu for .popover.

---
+++ WeatherApp/Views/SettingsMenu.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsMenu.swift
+//  WeatherApp
+//
+//  Created by Caleb Wolfe on 9/28/25.
+//
+
+import SwiftUI
+
+struct SettingsMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsMenu()
+}
