34fa03142d166293a46cad8c6aa1915adb041469
calebwolfe <caleb-wolfe@gmx.com>
2025-10-05 11:44:32 -0600

    Added skeleton for user SwiftData @Model.

---
+++ WeatherApp/Types/UserRecord.swift
@@ -0,0 +1,16 @@
+//
+//  UserRecord.swift
+//  WeatherApp
+//
+//  Created by Caleb Wolfe on 10/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class UserRecord {
+    var isFarenhiet: Bool = true
+    
+    init(){}
+}
