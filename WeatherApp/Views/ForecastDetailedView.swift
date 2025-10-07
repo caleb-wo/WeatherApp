c7ef7f6f1e0bf59a4b175e16b551abdb7232c72f
calebwolfe <caleb-wolfe@gmx.com>
2025-10-07 09:46:20 -0600

    Added file for detailed view.

---
+++ WeatherApp/Views/ForecastDetailedView.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastDetailedView.swift
+//  WeatherApp
+//
+//  Created by Caleb Wolfe on 10/7/25.
+//
+
+//import SwiftUI
+//
+//struct ForecastDetailedView: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//#Preview {
+//    ForecastDetailedView()
+//}
